30d18b1b68d93ee90ffff044849a10c26b416f3f
Jacob Gillespie <me@jacobwg.com>
2012-02-04 11:25:22 -0600

    typo

--- bin/run_minecraft.sh
+++ bin/run_minecraft.sh
@@ -2,8 +2,8 @@
 . /app/bin/sync-from-s3.sh
 echo "`cat /app/server.properties1`\nserver-port=$PORT\n`cat /app/server.properties2`" > /app/server.properties
 cd /app
-chmod +x /app/poor-mans-cron.sh
-/app/poor-mans-cron.sh &
+chmod +x /app/bin/poor-mans-cron.sh
+/app/bin/poor-mans-cron.sh &
 echo $! > cron.pid
 java -Xmx512M -Xms512M -jar /app/bin/minecraft_server.jar nogui
 kill `cat cron.pid`
