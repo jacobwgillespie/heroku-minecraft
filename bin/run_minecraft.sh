11aa6f82f6eb2793b27e0a3f84baaa868644e830
Jonathan Dance <jd@wuputah.com>
2012-07-17 03:00:18 -0700

    dont need chmod now

--- bin/run_minecraft.sh
+++ bin/run_minecraft.sh
@@ -10,9 +10,7 @@ cd /app
 
 . /app/bin/sync-from-s3.sh
 echo "`cat /app/server.properties1`\nserver-port=$PORT\n`cat /app/server.properties2`" > /app/server.properties
-chmod +x /app/bin/poor-mans-cron.sh
 /app/bin/poor-mans-cron.sh &
 echo $! > /app/cron.pid
-
 
 java -Xmx512M -Xms512M -jar /app/bin/minecraft_server.jar nogui
