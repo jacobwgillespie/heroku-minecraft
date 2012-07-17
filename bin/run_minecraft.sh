48e5876df359aa8ae762d70921a4975b359b2276
Jonathan Dance <jd@wuputah.com>
2012-07-17 02:57:24 -0700

    add sigterm handler

--- bin/run_minecraft.sh
+++ bin/run_minecraft.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+
+function shut_down {
+  kill `cat /app/cron.pid`
+  . /app/bin/sync-from-s3.sh
+}
+
+trap shut_down SIGTERM
+cd /app
+
 . /app/bin/sync-from-s3.sh
 echo "`cat /app/server.properties1`\nserver-port=$PORT\n`cat /app/server.properties2`" > /app/server.properties
-cd /app
 chmod +x /app/bin/poor-mans-cron.sh
 /app/bin/poor-mans-cron.sh &
-echo $! > cron.pid
+echo $! > /app/cron.pid
+
+
 java -Xmx512M -Xms512M -jar /app/bin/minecraft_server.jar nogui
-kill `cat cron.pid`
-. /app/bin/sync-to-s3.sh
