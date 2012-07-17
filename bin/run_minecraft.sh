c67a47755e8e24b533cf6b2395adad3ddc44e6a6
Jonathan Dance <jd@wuputah.com>
2012-07-17 03:03:38 -0700

    cleanup & comments

--- bin/run_minecraft.sh
+++ bin/run_minecraft.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-function shut_down {
+function shut_down() {
   kill `cat /app/cron.pid`
   . /app/bin/sync-from-s3.sh
 }
-
 trap shut_down SIGTERM
-cd /app
 
+# sync initial files
 . /app/bin/sync-from-s3.sh
-echo "`cat /app/server.properties1`\nserver-port=$PORT\n`cat /app/server.properties2`" > /app/server.properties
+
+# setup background syncing
 /app/bin/poor-mans-cron.sh &
 echo $! > /app/cron.pid
 
+# create server config
+cp /app/server.properties1 /app/server.properties
+echo "server-port=$PORT" >> /app/server.properties
+cat /app/server.properties2 >> /app/server.properties
+
+# run minecraft
+cd /app
 java -Xmx512M -Xms512M -jar /app/bin/minecraft_server.jar nogui
