450a796c03319160c6834a6ec04c3bb5d7a97c9b
Wil Gieseler <wil@wilgieseler.com>
2013-12-24 23:21:23 -0800

    sync server config files as well

--- bin/sync-from-s3.sh
+++ bin/sync-from-s3.sh
@@ -1,1 +1,6 @@
-boto-rsync -a $AWS_KEY -s $AWS_SECRET s3://$S3_BUCKET/world /app/world
+declare -a arr=(world server.properties banned-ips.txt banned-players.txt ops.txt white-list.txt)
+
+for i in ${arr[@]}
+do
+  boto-rsync -a $AWS_KEY -s $AWS_SECRET s3://$S3_BUCKET/$i /app/$i
+done
--- bin/sync-to-s3.sh
+++ bin/sync-to-s3.sh
@@ -1,1 +1,6 @@
-boto-rsync -a $AWS_KEY -s $AWS_SECRET /app/world s3://$S3_BUCKET/world
+declare -a arr=(world server.properties banned-ips.txt banned-players.txt ops.txt white-list.txt)
+
+for i in ${arr[@]}
+do
+  boto-rsync -a $AWS_KEY -s $AWS_SECRET /app/$i s3://$S3_BUCKET/$i
+done
