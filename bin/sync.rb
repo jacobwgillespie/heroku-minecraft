d51a824cd0b83608dd35aca2b1833390d9920ed7
Jacob Gillespie <jacobwgillespie@gmail.com>
2015-03-18 10:20:53 -0500

    Update ruby version, dependencies, and style

--- bin/sync.rb
+++ bin/sync.rb
@@ -1,7 +1,12 @@
 class Sync
-
-  WORLD = ['world']
-  ADMIN = ['server.properties', 'banned-ips.txt', 'banned-players.txt', 'ops.txt', 'white-list.txt']
+  WORLD = ["world"]
+  ADMIN = [
+    "server.properties",
+    "banned-ips.txt",
+    "banned-players.txt",
+    "ops.txt",
+    "white-list.txt"
+  ]
 
   def self.initial_sync
     puts "[sync] Performing initial S3 sync..."
@@ -26,23 +31,23 @@ class Sync
   end
 
   def self.push_to_s3(filename)
-    `boto-rsync -a $AWS_KEY -s $AWS_SECRET /app/#{filename} s3://$S3_BUCKET/#{filename}`
+    `boto-rsync -a $AWS_KEY -s $AWS_SECRET \
+      /app/#{filename} s3://$S3_BUCKET/#{filename}`
   end
 
   def self.pull_from_s3(filename)
-    `boto-rsync -a $AWS_KEY -s $AWS_SECRET s3://$S3_BUCKET/#{filename} /app/#{filename}`
+    `boto-rsync -a $AWS_KEY -s $AWS_SECRET \
+      s3://$S3_BUCKET/#{filename} /app/#{filename}`
   end
-
 end
 
 # Run
-
 if ARGV[0] == "init"
   Sync.initial_sync
 elsif ARGV[0] == "repeating"
   sleeptime = ARGV[1] ? ARGV[1].to_i : 250
   puts "Scheduling sync every #{sleeptime} seconds"
-  while true
+  loop do
     Sync.normal_sync
     sleep sleeptime
   end
