85f50209d05c31b36140dd3042aa384472b29641
Jacob Gillespie <jacobwgillespie@gmail.com>
2015-03-18 10:53:25 -0500

    Fix minecraft downloading

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -5,4 +5,4 @@ MC_VERSION="${MC_VERSION:-1.8.3}"
 
 
 # Download Minecraft
-curl -o "vendor/minecraft_server.jar https://s3.amazonaws.com/Minecraft.Download/versions/${MC_VERSION}/minecraft_server.${MC_VERSION}.jar"
+curl -o vendor/minecraft_server.jar "https://s3.amazonaws.com/Minecraft.Download/versions/${MC_VERSION}/minecraft_server.${MC_VERSION}.jar"
